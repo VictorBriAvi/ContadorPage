f02427eb4e5d0b2f6c079d262d82e801e8898bac
Victor Avila <victor_15_avila@hotmail.es>
2023-01-12 15:17:33 -0300

    Primera subida de contador

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import logito from './imagenes/Logotipo Original (PNG).png'
+import Boton from './componentes/Boton'
+import Contador from './componentes/Contador';
+import { useState } from 'react';
+
 
 function App() {
+
+  const  [numClicks, setNumClicks] = useState(0);
+
+  const manejarClick = () => {
+    setNumClicks(numClicks + 1);
+  };
+
+  const reinciarContador = () => {
+    setNumClicks(0);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo_contenedor">
+        <img 
+          className="logo__peluqueria"
+          src={logito}
+          alt="Logo de timePeluqueria"/>
+      </div>
+
+      <div className="contenedor__principal">
+        <Contador 
+          numeroDeClicks={numClicks} />
+        <Boton
+          texto="Click"
+          esBotonClick={true}
+          manejarClick={manejarClick} />
+
+        <Boton
+          texto="Reinciar"
+          esBotonDeClick={false}
+          manejarClick={reinciarContador} />
+
+      </div>
     </div>
   );
 }
---
+++ src/componentes/Contador.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../hojas-de-estilos/Contador.css"
+
+function Contador({ numeroDeClicks }) {
+
+    return (
+        <div className="contador">
+            {numeroDeClicks}
+
+        </div>
+    );
+    
+}
+
+export default Contador;
